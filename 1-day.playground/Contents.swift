54b53ec27c06124547af0c985914cf782685f5de
FutureDevelopersuz <107317980+BoBuRKhaN7475@users.noreply.github.com>
2022-06-12 00:41:59 +0500

    1 - day)
    Today i learned Let Constants and Var variable

---
+++ 1-day.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+import Foundation
+
+var date = Date()
+
+print(date)
+
+var name = "Bobur"
+name = "Farkhod"
+name = "Abdumutalib"
+
+let character = "SE2"
+
+
+let managerName = "Boburkhan"
+let dogBreed = "Hello"
+let meaningOfLife = "how many roads"
