8312801e1194efde5e0870f1412fff0d0401634a
FutureDevelopersuz <107317980+BoBuRKhaN7475@users.noreply.github.com>
2022-06-13 15:48:09 +0500

    Booleans , strings together simple data

---
+++ 2-day.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+
+let goodDogs = true
+var gameOver = false
+print(gameOver)
+gameOver.toggle()
+print(gameOver)
+
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+let name = "Bobur"
+let age = 22
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+let number = 11
+let misol = "Raqamni ko'rsat \(number)"
+
+print("5 x 5 is \(5 * 5)")
+
+
+
+let issiqlik = 40.0
+let kechqurunsovuqlik = (issiqlik * 9 / 9) + 32.0
+print("It was very hot today for example \(issiqlik) C  and \(kechqurunsovuqlik) F  esa shuncha")
