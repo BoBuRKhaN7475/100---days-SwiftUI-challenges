227564b0cbc15a532cc3e459c50e811ab01b3b57
FutureDevelopersuz <107317980+BoBuRKhaN7475@users.noreply.github.com>
2022-06-18 23:33:18 +0500

    last update 4-day

--- 4-Day.playground/Contents.swift
+++ 4-Day.playground/Contents.swift
@@ -2,4 +2,39 @@ import UIKit
 
 let surname: String = "Bobur"
 
-var score: Double = 0
+var score: Int = 55
+
+var dobl: Double = 5.5
+
+var IsAuthenticated: Bool = true
+
+
+var soda: [String] = ["Coke", "Pepsi", "Irn-Bru"]
+var teams: [String] = [String]()
+var cities: [String] = []
+var clues = [String] ()
+
+var albums: [String] = ["Red", "Fearless"]
+var user: [String: String] = ["id": "@twostraws"]
+var books: Set<String> = Set(["The Bluest Eye", "Foundation", "Girl, Woman, Other"])
+
+enum UIStyle {
+    case light, dark, system
+    
+}
+
+var style: UIStyle = UIStyle.system
+style = .dark
+
+let username: String
+// lots of complex logic
+username = "@twostraws"
+//lots more complex logic
+print(username)
+
+
+let stylex: UIStyle = UIStyle.system
+
+let scores: Int = 0
+
+var percentage: Double = 5.5
