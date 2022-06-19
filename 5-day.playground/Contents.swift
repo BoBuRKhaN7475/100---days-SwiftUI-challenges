027d4dab371d60de8decbf80247e9de71a04b7a9
FutureDevelopersuz <107317980+BoBuRKhaN7475@users.noreply.github.com>
2022-06-20 00:15:01 +0500

    If only

--- 5-day.playground/Contents.swift
+++ 5-day.playground/Contents.swift
@@ -1,9 +1,86 @@
 import UIKit
 
-var greeting: [String] = ["Hello","playground","qweq"]
+let score = 100
 
-print(greeting.count)
+if score > 80 {
+    print("Great")
+}
+let speed = 88
+let percentage = 85
+let age = 18
 
-greeting.removeAll(keepingCapacity:true)
+if speed >= 88 {
+    print("Where we're going")
+}
 
-print(greeting.count)
+if percentage < 85 {
+    print("Sorry you failed the test")
+}
+if age >= 18 {
+    print("You're eligible to vote.")
+}
+
+let OurName = "Dave Lister"
+let friendName = "Arnold Rimmer"
+
+if OurName < friendName {
+    print("It's \(OurName) vs \(friendName)")
+}
+
+if OurName > friendName {
+    print("its \(friendName) vs \(OurName)")
+}
+
+var numbers = [1,2,3]
+numbers.append(4)
+
+if numbers.count > 3 {
+    numbers.remove(at: 0)
+}
+print(numbers)
+
+let country = "Canada"
+
+
+if country == "Australia" {
+    print("G' day!")
+}
+
+var username = "taylorswift"
+
+if username.isEmpty == true {
+    username = "Anonymous"
+}
+
+print("Welcome , \(username)")
+
+
+
+let firstName = "Paul"
+let secondName = "Sophie"
+
+let firstAge = 40
+let secondAge = 10
+
+print(firstName == secondName)
+print(firstName != secondName)
+print(firstName < secondName)
+print(firstName >= secondName)
+
+print(firstAge == secondAge)
+print(firstAge != secondAge)
+print(firstAge < secondAge)
+print(firstAge >= secondAge)
+
+
+enum Sizes: Comparable {
+    case small
+    case medium
+    case large
+    
+}
+
+let first = Sizes.small
+let second = Sizes.large
+print(first > second)
+
